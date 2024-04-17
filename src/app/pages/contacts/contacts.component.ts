7af04c2ef705cbe38e118c4b1a1c162296b0bc07
Marcos Torres <marcostor13@gmail.com>
2024-04-16 20:56:51 -0500

    update: contacts

--- src/app/pages/contacts/contacts.component.ts
+++ src/app/pages/contacts/contacts.component.ts
@@ -5,6 +5,7 @@ import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzTableModule } from 'ng-zorro-antd/table';
 import { Contacts } from './interfaces/contacts.interface';
 import { ContactsService } from './services/contacts.service';
+import { NzModalModule } from 'ng-zorro-antd/modal';
 
 @Component({
   selector: 'app-contacts',
@@ -13,7 +14,8 @@ import { ContactsService } from './services/contacts.service';
     NzInputModule,
     FormsModule,
     NzButtonModule,
-    NzTableModule
+    NzTableModule,
+    NzModalModule
   ],
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.scss'
@@ -21,6 +23,9 @@ import { ContactsService } from './services/contacts.service';
 export class ContactsComponent {
   value: string = ''
   listOfData: Contacts[] = [];
+  isVisible: boolean = false
+  name: string = ''
+  phone: string = ''
 
   constructor(
     private contactsServices: ContactsService
@@ -34,6 +39,24 @@ export class ContactsComponent {
     this.contactsServices.getContacts().subscribe((contacts: Contacts[])=>{
       this.listOfData = contacts
     })
+  }
+
+  handleCancel(){
+    this.isVisible = false
+  }
+
+  handleOk(){
+    //llamar al servicio
+    const payload = {
+      name: this.name,
+      phone: this.phone
+    }
+    console.log(payload)
+    this.isVisible = false
+  }
+
+  openModal(){
+    this.isVisible = true
   }
 
 }
